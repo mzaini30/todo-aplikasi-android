0b870d0f4dd6cca2c323f0dff18b14e8873efc2c
mzaini30 <muhzaini30@gmail.com>
2024-01-23 22:50:26 +0800

    upload

--- src/Halaman/Beranda.svelte
+++ src/Halaman/Beranda.svelte
@@ -1,5 +1,7 @@
 <script>
   import { Input, Label, Tabs, TabItem, Helper, Toggle } from "flowbite-svelte";
+  import Gerbang from "../vendor/kunci/src/Gerbang.svelte";
+
   let inputanBaru = "";
   //   data[] -> todo, isDone
   let data = [];
@@ -19,53 +21,55 @@
   }
 </script>
 
-{#if data.length > 0}
-  <Tabs
-    contentClass="bg-white"
-    class="sticky top-0 left-0 bg-white z-[10]"
-    style="underline"
-  >
-    <TabItem open title="❌">
-      <div class="mb-[50px] p-5">
-        {#each data as item}
-          {#if item.isDone == false}
-            <Toggle
-              class="text-lg mb-5 last:mb-0"
-              on:change={() => {
-                item.isDone = !item.isDone;
-                localStorage.data = JSON.stringify(data);
-              }}
-              checked={item.isDone}>{item.todo}</Toggle
-            >
-          {/if}
-        {/each}
-      </div>
-    </TabItem>
-    <TabItem title="✔️">
-      <div class="mb-[50px] p-5">
-        {#each data as item}
-          {#if item.isDone == true}
-            <Toggle
-              class="text-lg mb-5 last:mb-0"
-              on:change={() => {
-                item.isDone = !item.isDone;
-                localStorage.data = JSON.stringify(data);
-              }}
-              checked={item.isDone}>{item.todo}</Toggle
-            >
-          {/if}
-        {/each}
-      </div>
-    </TabItem>
-  </Tabs>
-{/if}
-<form on:submit|preventDefault={onAdd} class="fixed w-full bottom-0 left-0">
-  <Input
-    type="text"
-    class="rounded-none"
-    id="first_name"
-    placeholder="New TODO"
-    required
-    bind:value={inputanBaru}
-  />
-</form>
+<Gerbang namaAplikasi="TODO">
+  {#if data.length > 0}
+    <Tabs
+      contentClass="bg-white"
+      class="sticky top-0 left-0 bg-white z-[10]"
+      style="underline"
+    >
+      <TabItem open title="❌">
+        <div class="mb-[50px] p-5">
+          {#each data as item}
+            {#if item.isDone == false}
+              <Toggle
+                class="text-lg mb-5 last:mb-0"
+                on:change={() => {
+                  item.isDone = !item.isDone;
+                  localStorage.data = JSON.stringify(data);
+                }}
+                checked={item.isDone}>{item.todo}</Toggle
+              >
+            {/if}
+          {/each}
+        </div>
+      </TabItem>
+      <TabItem title="✔️">
+        <div class="mb-[50px] p-5">
+          {#each data as item}
+            {#if item.isDone == true}
+              <Toggle
+                class="text-lg mb-5 last:mb-0"
+                on:change={() => {
+                  item.isDone = !item.isDone;
+                  localStorage.data = JSON.stringify(data);
+                }}
+                checked={item.isDone}>{item.todo}</Toggle
+              >
+            {/if}
+          {/each}
+        </div>
+      </TabItem>
+    </Tabs>
+  {/if}
+  <form on:submit|preventDefault={onAdd} class="fixed w-full bottom-0 left-0">
+    <Input
+      type="text"
+      class="rounded-none"
+      id="first_name"
+      placeholder="New TODO"
+      required
+      bind:value={inputanBaru}
+    />
+  </form>
+</Gerbang>
